b5dd82badd1eacbb64395871680f964bfc823bfb
mag1i <48532682+mag1i@users.noreply.github.com>
2022-07-04 15:11:17 +0100

    first commit

---
+++ app/src/main/java/com/example/arkids/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.arkids;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
